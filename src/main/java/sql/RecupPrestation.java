2695f5ffeb36b2fc7510fae10da7c9d6aca34c54
2barle <alexis51210@outlook.fr>
2022-05-27 10:47:13 +0200

    Préstations

--- src/main/java/sql/RecupPrestation.java
+++ src/main/java/sql/RecupPrestation.java
@@ -21,7 +21,8 @@ public class RecupPrestation {
                     "jdbc:postgresql://localhost:5432/bd_projetService","postgres","admin");
 
             //étape 3: créer l'objet statement
-            String query = "SELECT date_prestation,realise,description_prestation,note_prestation,c.nom_categorie,pers.nom_personne FROM prestation p " +
+            String query = "SELECT date_prestation,realise,description_prestation,note_prestation,c.nom_categorie,p.id_personne,pers.nom_personne,p.id_personne_1 " +
+                    "FROM prestation p " +
                     "join utilisateur u on p.id_personne=u.id_personne "+
                     "join personne pers on pers.id_personne=u.id_personne "+
                     "join categorie c on u.id_categorie=c.id_categorie "+
@@ -29,8 +30,7 @@ public class RecupPrestation {
             PreparedStatement updateSales = con.prepareStatement(query);
             updateSales.setInt(1, Integer.parseInt(id));
             updateSales.setInt(2, Integer.parseInt(id));
-
-            //étape 4: exécuter la requête
+             //étape 4: exécuter la requête
             ResultSet res = updateSales.executeQuery();
 
             int compteur = 1;
@@ -41,13 +41,38 @@ public class RecupPrestation {
                 arrayInfo.add(2,res.getString(3));
                 arrayInfo.add(3,res.getString(4));
                 arrayInfo.add(4,res.getString(5));
-                arrayInfo.add(5,res.getString(6));
+                if (res.getString(6).equals(id)) {
+                    arrayInfo.add(5,"Moi");
+                }else {
+                    arrayInfo.add(5,res.getString(7));
+                }
 
                 prestationsRetournees.put(compteur,arrayInfo.clone());
-
                 arrayInfo.clear();
                 compteur++;
             }
+
+/*
+
+            // etape 3 bis
+            String query2 =  "SELECT nom_personne FROM personne p " +
+                    "join utilisateur u on p.id_personne = u.id_personne " +
+                    "join prestation presta on presta.id_personne=u.id_personne " +
+                    "where presta.id_personne = ?";
+            PreparedStatement updateSales2 = con.prepareStatement(query2);
+            updateSales2.setInt(1, Integer.parseInt(id));
+            //étape 4 bis: exécuter la requête
+            ResultSet res2 = updateSales2.executeQuery();
+
+            int compteur2 = 1;
+
+            while(res2.next()) {
+
+                System.out.println(compteur2);
+                compteur2++;
+
+            }*/
+            System.out.println(prestationsRetournees);
             //étape 5: fermez l'objet de connexion
             con.close();
         }
